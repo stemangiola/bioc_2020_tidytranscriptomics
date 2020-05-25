e2da376727f1d1ade884d53da898d98974064a71
stemangiola <mangiolastefano@gmail.com>
2020-05-25 18:06:18 +1000

    update workshop

---
+++ R/tidytranscriptomics-package.R
@@ -0,0 +1,16 @@
+
+#' TidyTranscriptomics
+#' 
+#' @import stats
+#' @import utils
+#' @import tibble
+#' @import stringr
+#' @import ggplot2
+#' @import dplyr
+#' @import tidyr
+#' @import edgeR
+#' @import tidybulk
+#' @import tidyHeatmap
+#' @import ggrepel
+#' 
+i = 0
