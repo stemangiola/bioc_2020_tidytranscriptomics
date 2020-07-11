0b2498344b0b0f555fc8305190e3f1e0a319c4f2
Maria Doyle <maria.doyle@petermac.org>
2020-07-11 20:09:11 +1000

    Add pasilla dataset for exercises

---
+++ data-raw/pasilla.R
@@ -0,0 +1,16 @@
+## code to prepare pasilla SummarizedExperiment object
+
+countsfile <- system.file( "extdata/pasilla_gene_counts.tsv", package="pasilla" )
+counts <- read.table(countsfile, header=TRUE, row.names=1)
+colnames(counts) <- gsub("treated", "trt", colnames(counts))
+
+sampleinfo = data.frame(
+  row.names = colnames(counts), 
+  condition = c( "untreated", "untreated", "untreated",
+                 "untreated", "treated", "treated", "treated" ), 
+  type = c( "single_end", "single_end", "paired_end",
+            "paired_end", "single_end", "paired_end", "paired_end" ) )
+
+pasilla <- SummarizedExperiment::SummarizedExperiment(assays=list(counts=counts), colData=sampleinfo)
+
+usethis::use_data(pasilla, overwrite = TRUE)
